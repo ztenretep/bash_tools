3181b70735679e197e1b2257c5e0f73a88471a32
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-25 18:42:52 +0200

    Update remove_trailing_whitespace.bash

--- remove_trailing_whitespace.bash
+++ remove_trailing_whitespace.bash
@@ -3,6 +3,12 @@
 # Get filename from command line argument.
 FN=$1
 
+# Check if filename was given.
+if [ "${FN}" = '' ]; then
+    echo "No filename given. Bye!"
+    exit 1
+fi
+
 # Remove trailing whitespaces from file.
 sed -i 's/[[:space:]]*$//' "${FN}"
 
