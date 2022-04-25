8656aade378e2edccf1d35d404e5391680520c20
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-25 19:43:01 +0200

    Update remove_trailing_whitespace.bash

--- remove_trailing_whitespace.bash
+++ remove_trailing_whitespace.bash
@@ -1,15 +1,24 @@
 #!/usr/bin/bash
-
-# One can use standard Posix bracket Expression for removal:
+#
+# The script removes only trailing whitespace from a given file. 
+# This is a standard task for python as well as for shell scripts.
+#
+# The shell command sed is used for the task. The trailing
+# whitespaces are in situ removed inside the file.
+#
+# Within sed one can use standard Posix bracket expression for
+# the removal:
 # [[:space:]] = all whitespace characters including line breaks
 # [[:blank:]] = space and tab
 
 # Get the filename from the command line argument.
 FN=$1
 
-# Check if filename was given.
+# Check if filename was given. Otherwise exit script.
 if [ "${FN}" = '' ]; then
+    # Print farewell message.
     echo "No filename given. Bye!"
+    # Exit script with error.
     exit 1
 fi
 
