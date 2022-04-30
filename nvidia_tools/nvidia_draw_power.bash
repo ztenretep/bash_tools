dcb2da61f8471cd483e663d891f5578c42b01c60
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-30 10:07:54 +0200

    Update nvidia_draw_power.bash

--- nvidia_tools/nvidia_draw_power.bash
+++ nvidia_tools/nvidia_draw_power.bash
@@ -1,10 +1,23 @@
 #!/usr/bin/bash
+#
+# The nvidia-smi command is shipped with the graphics card driver from Nvidia.
+# After setting the power of an installed Nvidia graphics card, this script
+# displays the current power consumption at an interval of 1000 milliseconds.
 
-# Set grafic card number
+# Set the grafic card number to monitor.
 DEV=0
 
-# Print power draw of Nvidia grafic card to screen.
-nvidia-smi -i $DEV --query-gpu=power.draw --format=csv --loop-ms=1000
+# Function which is called from trap.
+trap_ctrl_c() {
+    echo '<CTRL+C> has been trapped!'
+    exit 1
+}
+
+# Set up trap for CTRL+C.
+trap trap_ctrl_c
+
+# Print the current power draw of the Nvidia grafic card to the screen.
+nvidia-smi -i ${DEV} --query-gpu=power.draw --format=csv --loop-ms=1000
 
 # Exit script without error.
 exit 0
