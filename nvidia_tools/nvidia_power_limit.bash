d98d07c4378dcd927d8003c9c87c888a927acdf8
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-01 09:36:26 +0200

    Update nvidia_power_limit.bash

--- nvidia_tools/nvidia_power_limit.bash
+++ nvidia_tools/nvidia_power_limit.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 #
+# The command nvidia-smi is part of the NVIDIA driver package installation.
+#
 # E.g. A NVIDIA GeForce RTX 3070 Ti OC can vary power in a range of 100 Watt to 320 Watt.
 # Setting power limit to 100 Watt reduce the power draw in process to 100 Watt or less.
 
